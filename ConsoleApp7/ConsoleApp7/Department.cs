8bfe6d96857b062f2fa28f788ddd7ee087feef17
zeynallisahile <tu7dfbyuu@code.edu.az>
2022-03-18 12:02:08 +0400

    Task Completed

---
+++ ConsoleApp7/ConsoleApp7/Department.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    internal class Department
+    {
+        public string Name;
+        private int _employeeLimit;
+        private int _salaryLimit;
+        public Employee[] Employees = new Employee[0];
+        public int EmployeeLimit
+        {
+            get
+            {
+                return _employeeLimit;
+            }
+            set
+            {
+                if (value >= 2 && value <= 4)
+                    _employeeLimit = value;
+            }
+        }
+        public int SalaryLimit
+        {
+            get
+            {
+                return _salaryLimit;
+            }
+            set
+            {
+                if (value == 250)
+                    _salaryLimit = value;
+            }
+        }
+        public void AddEmployee(Employee employee)
+        {
+
+            if (Employees.Length == EmployeeLimit)
+                throw new Exception("Limit doldu");
+         
+        }
+    }
+}
+
---
+++ ConsoleApp7/ConsoleApp7/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    internal class Employee
+    {
+
+        private string _name;
+        private string _surName;
+        private int _salary;
+        public string EmployeeName
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (Checker(value))
+                    _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surName;
+            }
+            set
+            {
+                if (Checker(value))
+                    _surName = value;
+            }
+        }
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value >= 250)
+                    _salary = value;
+            }
+        }
+        public string GetInfo()
+        {
+            return $"Ad: {this._name} Soyad: {this._surName} Maas: {this._salary} ";
+        }
+        public bool Checker(string value)
+        {
+            int count = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsLetter(value[i]))
+                    count++;
+            }
+            if (count == value.Length && char.IsUpper(value[0]) && !string.IsNullOrWhiteSpace(value))
+                return true;
+            return false;
+        }
+    }
+}
+
